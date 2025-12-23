2eac4041ba1ec57ad02576f8f594f67ee64b2da9
XMKT <bezrukov005@gmail.com>
2025-12-23 22:51:13 +0300

    4. Основы синтаксиса PHP

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo "I love pizza<br>";
+// Это однострочный комментарий
+/*
+Это многострочный комментарий
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Order a pizza</button>
+</body>
+</html>
