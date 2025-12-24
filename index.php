71f8fa21694cb2a4aca16e4606e1bf69c182bb0d
XMKT <bezrukov005@gmail.com>
2025-12-24 04:09:54 +0300

    6. Арифметические операторы, инкремент/декремент

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-$name = "Bro";
-$age = 21;
-$price = 4.99;
-$is_online = false;
-echo "Hello {$name}<br>";
-echo "You are {$age} years old<br>";
+$x = 10;
+$y = 2;
+$z = $x + $y;
+echo $z; // 12
+$counter = 0;
+$counter++;
+echo $counter; // 1
 ?>
 
 <!DOCTYPE html>
