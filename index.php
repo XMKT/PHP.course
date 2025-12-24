fc234e6c426398f7d4fd66aedccb30993993f7c1
XMKT <bezrukov005@gmail.com>
2025-12-24 05:11:21 +0300

    19

--- index.php
+++ index.php
@@ -2,20 +2,15 @@
 $capitals = array(
 	"USA" => "Washington D.C.",
 	"Japan" => "Kyoto",
-	"South Korea" => "Seoul",
 	"India" => "New Delhi"
 );
-$capitals["USA"] = "Las Vegas";
-$capitals["China"] = "Beijing";
-foreach ($capitals as $key => $value) {
-	echo "{$key} = {$value}<br>";
-}
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$country = $_POST['country'];
-	$capital = $capitals[$country] ?? "Capital not found.";
+	$capital = $capitals[$country];
 	echo "The capital of {$country} is {$capital}.";
 }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
