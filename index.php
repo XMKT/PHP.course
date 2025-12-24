70ac6844777bfa0c18bf3fe5a5146a6e183ca6c0
XMKT <bezrukov005@gmail.com>
2025-12-24 04:09:17 +0300

    5. Переменные и типы данных

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <?php
-echo "I love pizza<br>";
-// Это однострочный комментарий
-/*
-Это многострочный комментарий
-*/
+$name = "Bro";
+$age = 21;
+$price = 4.99;
+$is_online = false;
+echo "Hello {$name}<br>";
+echo "You are {$age} years old<br>";
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
 </head>
+
 <body>
-    <button>Order a pizza</button>
 </body>
+
 </html>
