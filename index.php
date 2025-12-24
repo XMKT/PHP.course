0ad6820dc79cd8e544a14f94bcd3af946def9ed7
XMKT <bezrukov005@gmail.com>
2025-12-24 05:41:21 +0300

    25. Функция filter_input()

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
-$username = " Bro Code ";
-$username = strtolower(trim($username));
-echo $username; // "bro code"
-$phone = "123-456-7890";
-$phone = str_replace("-", "", $phone);
-echo $phone; // "1234567890"
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+	$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+	if (empty($username)) {
+		echo "Username is required.";
+	}
+	if ($email === false) {
+		echo "Invalid email format.";
+	}
+}
 ?>
 
 <!DOCTYPE html>
