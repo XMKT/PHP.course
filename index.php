7769100e6bab180e0bd3f29d872355a1aec48f94
XMKT <bezrukov005@gmail.com>
2025-12-24 04:15:06 +0300

    10. Условные операторы if, else if и else

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$radius = $_POST['radius'];
-	$circumference = round(2 * pi() * $radius, 2);
-	$area = round(pi() * pow($radius, 2), 2);
-	$volume = round((4 / 3) * pi() * pow($radius, 3), 2);
-	echo "Circumference: {$circumference} cm<br>";
-	echo "Area: {$area} cm²<br>";
-	echo "Volume: {$volume} cm³<br>";
+$age = 25;
+if ($age >= 18 && $age < 100) {
+	echo "You may enter the site.";
+} else if ($age >= 100) {
+	echo "You are too old to enter.";
+} else {
+	echo "You must be 18+ to enter.";
 }
 ?>
 
@@ -20,12 +19,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 
 <body>
-	<form action="index.php" method="post">
-		<label>Radius:</label><br>
-		<input type="text" name="radius"><br>
-		<input type="submit" value="Calculate">
-	</form>
-
 </body>
 
 </html>
